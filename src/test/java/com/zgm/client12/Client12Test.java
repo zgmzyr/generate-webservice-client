c8ce3bdab05784547d8ab9dc917063b7076b6999
zgm <zgm@zgm.com>
2013-11-04 15:37:07 +0800

    用org.apache.cxf的cxf-codegen-plugin插件生成webservice客户端

---
+++ src/test/java/com/zgm/client12/Client12Test.java
@@ -0,0 +1,19 @@
+package com.zgm.client12;
+
+import java.net.MalformedURLException;
+
+import org.junit.Test;
+
+public class Client12Test {
+
+	@Test
+	public void test() throws MalformedURLException {
+		//TODO
+//		LotteryForJavaService service = new LotteryForJavaService(new URL("http://192.168.0.250/LotteryJava/LotteryForJavaService.svc?wsdl"));
+//		RedBlueBallIssueInfoResult result = service.getBasicHttpBindingILotteryForJavaService().getRedBlueBallIssueInfoByIssueNo("123456", null);
+		
+//		assertTrue("", result != null);
+		
+	}
+
+}
